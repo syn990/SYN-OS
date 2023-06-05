3a104310946119c3735520f2feb8b8430db257b1
syn990 <william@npc.syntax990.com>
2023-06-05 16:54:33 +0100

    Cleanup in various

--- SYN-OS-V4/SYN-INSTALLER-SCRIPTS/syn-installer-functions.sh
+++ SYN-OS-V4/SYN-INSTALLER-SCRIPTS/syn-installer-functions.sh
@@ -8,26 +8,34 @@ check_success() {
     fi
 }
 
-# Function to display a warning message and confirm the action
-confirm_action() {
-    local message=$1
-    local disclaimer=$'\n\n\e[1;33mWARNING:\e[0m By proceeding, you acknowledge and agree that William (Syntax990) assumes \e[1;33mNO responsibility\e[0m for ANY outcomes. \e[1;33mABSOLUTELY NO WARRANTIES\e[0m are provided. You are \e[1;33mFULLY accountable\e[0m for the consequences.'
+confirm_wipe() {
+	local message=$1
+    local disclaimer="WARNING: This operation will permanently delete all data on the specified device. There is no way to recover the data once it is wiped. You are FULLY responsible for any consequences."
 
-    echo -e "$disclaimer\n$message"
-    read -p "Do you want to proceed? (y/n) " confirm
+    echo -e "$disclaimer"
+    read -p "Do you want to proceed with the data wipe? (y/n) " confirm
     if [ "$confirm" != "y" ]; then
-        echo -e "\e[1;31mAutonomous judgment waiver execution has been halted. It is IMPERATIVE to comply with all tenets of the RTFM act!! MODIFY syn-stage0.sh immediately to ensure system viability and PREVENT IRREVOCABLE DATA DESTRUCTION!\e[0m"
+        echo -e "Data wipe operation has been canceled."
         exit 0
     fi
 }
 
 
 check_if_arch_repo_are_accessible() {
-    ping -c 1 archlinux.org >/dev/null 2>&1
-    if [ $? -eq 0 ]; then
-        echo "Network connectivity is available. Proceeding."
-    else
+    if ! ping -c 1 archlinux.org >/dev/null 2>&1; then
         echo "Error: Unable to reach Arch Linux repositories. Please check your network connection and try again."
         exit 1
     fi
+
+    if ! host -t SOA archlinux.org >/dev/null 2>&1; then
+        echo "Error: DNS resolution for Arch Linux repositories failed. Please check your DNS settings and try again."
+        exit 1
+    fi
+
+    if ! curl -s --head https://archlinux.org >/dev/null 2>&1; then
+        echo "Error: Unable to establish a secure connection to Arch Linux repositories. Please check your SSL/TLS configuration and try again."
+        exit 1
+    fi
+
+    echo "Network connectivity is available. Proceeding."
 }
--- SYN-OS-V4/SYN-INSTALLER-SCRIPTS/syn-pacstrap-variables.sh
+++ SYN-OS-V4/SYN-INSTALLER-SCRIPTS/syn-pacstrap-variables.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-# Below you will find all the packages used on the initial pacstrap before installing files to the main system.
-# You can insert package names found in the repositories here if you want them installed on the initial pacstrap.
-# Or simply scroll down to the pacstrap and add additional packages: "pacstrap /mnt $SYNSTALL *package_name_here*"
-
-# You can add/remove packages in these variables. It's done this way so you can see what's being installed.
-# This implementation means you can modify the script and even omit entire sections conveniently.
-
-# All packages are installed in a single pacstrap command, allowing a total-size prediction for all packages during install.
-# Ensure the package name is valid, and the mirrors can be read, and pacstrap will install it.
+# This script installs packages using pacstrap for the initial setup of the main system.
 
 
-BASE_990="base base-devel dosfstools fakeroot gcc linux linux-firmware pacman-contrib sudo zsh"
-SYSTEM_990__="alsa-utils archlinux-xdg-menu dhcpcd dnsmasq hostapd iwd pulseaudio python-pyalsa"
-CONTROL_990_="lxrandr obconf-qt pavucontrol-qt"
-WM_990______="openbox xcompmgr xorg-server xorg-xinit tint2"
-CLI_990_____="git htop man nano reflector rsync wget"
-GUI_990_____="engrampa feh kitty kwrite pcmanfm-qt"
-FONT_990____="terminus-font ttf-bitstream-vera"
-CLI_XTRA_990="android-tools archiso binwalk brightnessctl hdparm hexedit lshw ranger sshfs yt-dlp"
-GUI_XTRA_990="audacity chromium gimp kdenlive obs-studio openra spectacle vlc"
-#VM_XTRA_990_="edk2-ovmf libvirt qemu-desktop virt-manager virt-viewer"
+# The following variables contain the names of packages to be installed.
+# You can modify these variables to add or remove packages as needed.
 
-SYNSTALL="$BASE_990 $SYSTEM_990__ $CONTROL_990_ $WM_990______ $CLI_990_____ $GUI_990_____ $FONT_990____ $CLI_XTRA_990 $GUI_XTRA_990 $VM_XTRA_990_"
+basePackages="base base-devel dosfstools fakeroot gcc linux linux-firmware pacman-contrib sudo zsh"
+systemPackages="alsa-utils archlinux-xdg-menu dhcpcd dnsmasq hostapd iwd pulseaudio python-pyalsa"
+controlPackages="lxrandr obconf-qt pavucontrol-qt"
+wmPackages="openbox xcompmgr xorg-server xorg-xinit tint2"
+cliPackages="git htop man nano reflector rsync wget"
+guiPackages="engrampa feh kitty kwrite pcmanfm-qt"
+fontPackages="terminus-font ttf-bitstream-vera"
+cliExtraPackages="android-tools archiso binwalk brightnessctl hdparm hexedit lshw ranger sshfs yt-dlp"
+guiExtraPackages="audacity chromium gimp kdenlive obs-studio openra spectacle vlc"
+#vmExtraPackages="edk2-ovmf libvirt qemu-desktop virt-manager virt-viewer"
+
+# The SYNSTALL variable combines all the package variables for the pacstrap command.
+SYNSTALL="$basePackages $systemPackages $controlPackages $wmPackages $cliPackages $guiPackages $fontPackages $cliExtraPackages $guiExtraPackages"
+
+# Usage: pacstrap /mnt $SYNSTALL
+# This command installs all the packages listed in the SYNSTALL variable to the specified mount point.
+# Make sure the package names are valid and that the mirrors can be read.
+
+pacstrap /mnt $SYNSTALL
