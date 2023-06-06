949f4b0dc35ed0acd90126442b2c17463b47d7b3
syn990 <william@syntax990.com>
2023-06-06 02:08:32 +0100

    SYN-OS CONSOLIDATION - SYN-BUILDER

--- SYN-OS-V4/SYN-ISO-PROFILE/profiledef.sh
+++ SYN-OS-V4/SYN-ISO-PROFILE/profiledef.sh
@@ -1,25 +1,52 @@
+```bash
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
-iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+iso_name="SYN-OS"
+iso_label="SYN-OS"
+iso_publisher="Syntax990"
+desktop="Install Image"
+iso_application="SYN-RTOS-V4 Installation Enviroment"
+iso_version="V4"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
-           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+           'uefi-ia32.grub.esp' 'uefi-x64.systemd-boot.esp'
+           'uefi-ia32.grub.eltorito' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
+pacman_testing_conf="pacman-testing.conf"
+
+# The type of filesystem image to be created
+# Only squashfs is generally used for this purpose
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+#airootfs_image_type="alternative_filesystem"
+
+# Options to be passed to the image creation tool
+airootfs_image_tool_options=('-comp' 'lz4')
+
+# Different compression algorithms, commented out
+#airootfs_image_tool_options=('-comp' 'gzip')
+#airootfs_image_tool_options=('-comp' 'lzo')
+#airootfs_image_tool_options=('-comp' 'xz')
+#airootfs_image_tool_options=('-comp' 'lzma')
+#airootfs_image_tool_options=('-comp' 'zstd', '-Xcompression-level', '22')
+
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
-  ["/root/.automated_script.sh"]="0:0:755"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/root/syn-stage0.sh"]="0:0:750"
+  ["/root/syn-stage1.sh"]="0:0:750"
+  ["/root/SYN-INSTALLER-SCRIPTS/motd-primer.sh"]="0:0:750"
+  ["/root/SYN-INSTALLER-SCRIPTS/motd.sh"]="0:0:750"
+  ["/root/SYN-INSTALLER-SCRIPTS/syn-1_chroot.sh"]="0:0:750"
+  ["/root/SYN-INSTALLER-SCRIPTS/syn-ascii-art.sh"]="0:0:750"
+  ["/root/SYN-INSTALLER-SCRIPTS/syn-disk-variables.sh"]="0:0:750"
+  ["/root/SYN-INSTALLER-SCRIPTS/syn-installer-functions.sh"]="0:0:750"
+  ["/root/SYN-INSTALLER-SCRIPTS/SYN-INSTALLER-MAIN.sh"]="0:0:750"
+  ["/root/SYN-INSTALLER-SCRIPTS/syn-pacstrap-variables.sh"]="0:0:750"
+  ["/root/SYN-TOOLS/equip-profile-with-repo.sh"]="0:0:750"
+  ["/root/SYN-TOOLS/ShowInterfaceAddrLoop.sh"]="0:0:750"
+  ["/root/SYN-TOOLS/REBUILD_ISO.sh"]="0:0:750"
 )
+
