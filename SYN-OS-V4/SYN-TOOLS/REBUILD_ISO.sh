26086a7707e75542b232b4ae9a6ec0f6804e9975
Syntax990 <79884715+syn990@users.noreply.github.com>
2023-09-21 17:59:45 +0100

    Update REBUILD_ISO.sh

--- SYN-OS-V4/SYN-TOOLS/REBUILD_ISO.sh
+++ SYN-OS-V4/SYN-TOOLS/REBUILD_ISO.sh
@@ -1,7 +1,27 @@
-# This script automates the proceedure of deleting the previous work directory
-# as well as deleting the previous ISO (This is useful as it builds the ISO in the same DIR, ensuring
-# my test VM is simple and always looking at the latest ISO...
+#!/bin/bash
 
-rm -R /home/syntax990/SYN-OS/WORKDIR
-rm -R /home/syntax990/SYN-OS/*.iso
-mkarchiso -v -w WORKDIR -o /home/syntax990/SYN-OS /home/syntax990/SYN-OS/SYN-OS-V4/SYN-ISO-PROFILE/
+# Variables
+WORKDIR="/home/syntax990/SYN-OS/WORKDIR"
+ISODIR="/home/syntax990/SYN-OS"
+ISOPROFILE="/home/syntax990/SYN-OS/SYN-OS-V4/SYN-ISO-PROFILE/"
+
+# Check and remove existing WORKDIR
+if [ -d "$WORKDIR" ]; then
+    rm -R "$WORKDIR"
+    echo "The directory $WORKDIR has been deleted."
+else
+    echo "The directory $WORKDIR does not exist."
+fi
+
+# Check and remove existing ISO files
+for iso_file in "$ISODIR"/*.iso; do
+    if [ -f "$iso_file" ]; then
+        rm "$iso_file"
+        echo "The file $iso_file has been deleted."
+    else
+        echo "No ISO files found in $ISODIR."
+    fi
+done
+
+# Create a new ISO
+mkarchiso -v -w "$WORKDIR" -o "$ISODIR" "$ISOPROFILE"
