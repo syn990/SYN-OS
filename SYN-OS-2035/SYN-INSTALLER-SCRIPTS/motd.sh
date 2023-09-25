8f00d8f7bc22b08cd8f33ac9f2cd4ab96704bfec
syn990 <william@syntax990.com>
2023-09-25 23:18:48 +0100

    2035 general edits

--- SYN-OS-2035/SYN-INSTALLER-SCRIPTS/motd.sh
+++ SYN-OS-2035/SYN-INSTALLER-SCRIPTS/motd.sh
@@ -2,33 +2,62 @@
 
 printf "\n\e[1;32mWelcome to \e[1mSYN-OS\e[0;32m. This robust and advanced operating system marks the evolution of building your own operating system, based on \e[1mArch Linux\e[0;32m.\e[0m\n\n"
 
-printf "For installation enquiries, contact the creator, \e[1;33mSyntax990\e[0m at \e[1;34mwilliam@npc.syntax990.com\e[0m. Build materials can be obtained from our Github repository: \e[1;34mhttps://github.com/syn990/syn-rtos.git\e[0m\n\n"
 
-printf "01010011 01011001 01001110 00101101 01001111 01010011"
-printf "SYN-OS: The Syntax Operating System"
-printf ""
-printf "\e[1;31m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
-printf "     _______.____    ____ .__   __.          ______        _______.\n"
-printf "    /       |\   \  /   / |  \ |  |         /  __  \      /       |\n"
-printf "   |   (----  \   \/   /  |   \|  |  ______|  |  |  |    |    ---- \n"
-printf "    \   \      \_    _/   |  . \`  | |______|  |  |  |     \   \    \n"
-printf ".----    |       |  |     |  |\   |        |  \`--'  | .----    |   \n"
-printf "|_______/        |__|     |__| \__|         \______/  |_______/    \n"
-printf "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\e[0m\n\n"
 
-printf "In your \e[1;33m/root/\e[0m directory, you will find two significant scripts that require your attention:\n\n"
+#!/bin/bash
 
-printf "\e[1;33mSYN-INSTALLER-MAIN.sh\e[0m: This script prepares your \e[1mfilesystem\e[0m on the storage medium using your live host system. It installs the \e[1mpackages\e[0m specified within the script variables. Prior to execution, carefully read and adjust the script as necessary. Lack of discretion may lead to \e[1mdata loss\e[0m.\n\n"
+# Source additional functions
+source /root/SYN-OS-2035/SYN-INSTALLER-SCRIPTS/syn-installer-functions.sh
 
-printf "\e[1;33msyn-stage1.sh\e[0m: Once you verify there are no errors or unexpected glitches in the \e[1;33mSYN-INSTALLER-MAIN.sh\e[0m script, \e[1;33msyn-stage1.sh\e[0m should be run within the \e[1mchroot\e[0m environment. This script is automatically replicated to your environment.\n\n"
+# ANSI Colour Codes
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+RESET="\033[0m"
 
-printf "Execution of these scripts demands serious consideration of their functions. Failure to comprehend their impact may lead to \e[1mirreversible consequences\e[0m.\n\n"
+clear
 
-printf "Remember, \e[1msecurity\e[0m and thorough understanding are pivotal to the successful operation of \e[1mSYN-OS\e[0m.\n\n"
+# Display SYN-OS ASCII Art
+echo "${GREEN}"
+cat << "EOF"
+01010011 01011001 01001110 00101101 01001111 01010011
+SYN-OS: The Syntax Operating System
 
-printf "Your journey with \e[1mSYN-OS\e[0m begins here. Proceed with the commands as follows:\n\n"
+EOF
+echo "${RESET}"
 
-printf "systemctl start dhcpcd.serivce"
-printf "Otherwise complete the network configuration setup yourself" 
-printf ""
-printf "\e[1;34msh SYN-INSTALLER-MAIN.sh\n"
+# Welcome Message
+printf "${YELLOW}Welcome to SYN-OS.${RESET}\n"
+printf "This system is built on the robust architecture of Arch Linux.\n"
+
+# Script Context
+printf "\n${BLUE}You are currently in the SYN-OS installation environment.${RESET}\n"
+printf "The SYN-INSTALLER-MAIN.sh script has guided the system setup and initialisation.\n"
+printf "Execute that script to begin installation.\n"
+
+# Status and Tips
+printf "\n${RED}Status:${RESET}\n"
+
+# Check network connectivity to Arch Linux repositories
+check_if_arch_repo_are_accessible
+
+if [ $? -eq 0 ]; then
+    printf "• Arch Linux repositories accessible: ${GREEN}Online${RESET}\n"
+else
+    printf "• Arch Linux repositories accessible: ${RED}Offline${RESET}\n"
+fi
+
+printf "• SYN-OS components loaded: ${GREEN}Successful${RESET}\n"
+
+# What to do next
+printf "\n${YELLOW}Next Steps:${RESET}\n"
+printf "1. Use ${GREEN}arch-chroot${RESET} to enter the new environment.\n"
+printf "2. Run additional post-installation scripts as needed.\n"
+printf "3. Check logs and configurations before rebooting.\n"
+
+# Footer
+printf "\n${BLUE}Support and Resources:${RESET}\n"
+printf "• For installation enquiries, reach out to ${YELLOW}Syntax990${RESET} via email at ${BLUE}william@npc.syntax990.com${RESET}\n"
+printf "• Source code and build materials can be accessed on GitHub: ${BLUE}https://github.com/syn990/syn-rtos.git${RESET}\n"
+printf "${RED}Exercise caution while installing and configuring. Refer to the GitHub repository for documentation.${RESET}\n\n"
