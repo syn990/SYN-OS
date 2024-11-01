3b52b323aa5ae0f0bb77595f1766d14bfda5bb42
Syntax990 <79884715+syn990@users.noreply.github.com>
2024-11-01 11:21:08 +0000

    Improved description

--- SYN-OS/SYN-ISO-PROFILE/airootfs/root/syn-resources/scripts/syn-lanbridge.sh
+++ SYN-OS/SYN-ISO-PROFILE/airootfs/root/syn-resources/scripts/syn-lanbridge.sh
@@ -1,16 +1,16 @@
 #!/bin/zsh
 
-# SYN-OS
-# SYNTAX990
-# William Hayward-Holland
-# MIT License
+# =============================================================================
+#                                 SYN-OS lanbridge
+#       Script to Create a Wireless Bridge with Ethernet for NAT Sharing
+# -----------------------------------------------------------------------------
+#   This script enables Internet sharing from a wireless to an ethernet port,
+#   creating a NAT bridge as outlined on the ArchWiki.
+#   Author: William Hayward-Holland (Syntax990)
+#   License: MIT
+# =============================================================================
 
-# syn-lanbridge.sh
-
-# Create a wireless bridge between a wireless and ethernet port for NAT (Internet Sharing - ArchWiki).
-# Assumes manual connection to a wireless network with another computer/switch plugged in via ethernet.
-
-# Define interface variables detected on boot.
+# Define network interface variables
 WIRELESS_INTERFACE=wlan0
 ETHERNET_INTERFACE=eno1
 IP_ADDRESS=139.96.30.100/24
@@ -20,18 +20,18 @@ BROADCAST_ADDRESS=139.96.30.0/24
 ip link set up dev $ETHERNET_INTERFACE
 ip addr add $IP_ADDRESS dev $ETHERNET_INTERFACE
 
-# Enable Packet Forwarding in Kernel
+# Enable packet forwarding in the kernel
 sysctl net.ipv4.ip_forward=1
 
-# Ethernet to WiFi - Bridge / Forwarding
+# Configure NAT forwarding between Ethernet and WiFi
 iptables -t nat -A POSTROUTING -o $WIRELESS_INTERFACE -j MASQUERADE
 iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i $ETHERNET_INTERFACE -o $WIRELESS_INTERFACE -j ACCEPT
 
-# DHCP firewall rules Ethernet to WiFi 
+# Set up DHCP firewall rules for Ethernet to WiFi
 iptables -I INPUT -p udp --dport 67 -i $ETHERNET_INTERFACE -j ACCEPT
 iptables -I INPUT -p udp --dport 53 -s $BROADCAST_ADDRESS -j ACCEPT
 iptables -I INPUT -p tcp --dport 53 -s $BROADCAST_ADDRESS -j ACCEPT
 
-# Enable DHCP SERVER on ETHERNET
-systemctl start dhcpd4@$ETHERNET_INTERFACE.service 
+# Start DHCP server on Ethernet interface
+systemctl start dhcpd4@$ETHERNET_INTERFACE.service
